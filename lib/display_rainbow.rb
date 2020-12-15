db894b4df677951f85820063ae8246d12055d3cd
Kristyn Little <kristynlittle@frontier.com>
2020-12-15 02:15:00 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
-# Write your #display_rainbow method here
+colors=["red","orange","yellow","green","blue","indigo","violet"]
+
+def display_rainbow
+  puts "colors#{colors[0]},colors[1],colors[2],colors[3],colors[4],colors[5],colors[6]}"
+end
